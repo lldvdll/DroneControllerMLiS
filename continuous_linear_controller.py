3640431d532a1dfa54bb5136c449fb6ca68fe2de
lldvdll <dgs.analytics.ltd@gmail.com>
2026-01-14 21:05:30 +0000

    Added state, action, reward, and return calculations. Also forward policy method and get_thrusts, with the action to thrust method

--- continuous_linear_controller.py
+++ continuous_linear_controller.py
@@ -5,11 +5,12 @@ from drone import Drone
 
 
 class Policy():
-    def __init__(self):
-        pass
+    def __init__(self, input_size, output_size):
+        # Initialize weights to zeros
+        self.weights = np.zeros((input_size, output_size))        
     
     def forward(self, state):
-        pass
+        return state @ self.weights  # Linear model
     
     def backward(self):
         pass
@@ -22,32 +23,72 @@ class Policy():
 
 class ContinuousLinearController(FlightController):
     def __init__(self):
-        super().__init__()
-        
+                
         # Load config
         with open('continuous_linear_config.json', 'r') as f:
             self.config = json.load(f)
         
         # Initialise policy
-        self.policy = Policy()
+        input_size = self.config['input_size']
+        output_size = self.config['output_size']
+        self.policy = Policy(input_size, output_size)
 
     def get_state(self, drone: Drone):
         """
         Get state from drone object
         """
-        pass
+        # Get drone state parameters
+        target = drone.get_next_target()
+        dx = (target[0] - drone.x)
+        dy = (target[1] - drone.y)
+        vx = drone.velocity_x
+        vy = drone.velocity_y
+        pitch = drone.pitch
+        pitch_vel = drone.pitch_velocity
+        bias = 1.0  # Bias for linear model
+        
+        # Normalise - to help stabilise learning
+        state = np.array([dx, dy, vx, vy, pitch, pitch_vel, bias])
+        norms = np.array([2.0, 2.0, 5.0, 5.0, 1.0, 1.0, 1.0])
+        state = state / norms
+        
+        return state
     
     def get_action(self, state, mode='test'):
         """
         Get action from policy
+            mode='train': Add gaussian noise (exploration)
+            mode='test': Use mean directly (exploitation)
+        Return action pair: [total_thrust, roll]
         """
-        pass
+        # Get the mean (mu) from the policy
+        mu = self.policy.forward(state)
+        
+        if mode == 'train':
+            sigma = self.config['hyperparameters']['sigma']
+            action = np.random.normal(loc=mu, scale=sigma)
+            return action
+        else:
+            return mu
     
     def convert_action_to_thrust(self, action):
         """
         Convert action to thrusts
+            0: Total thrust
+            1: Roll
         """
-        pass
+        t_total = action[0]
+        roll = action[1]
+        
+        # Mixing logic to convert to thrusts
+        left = (t_total + roll) / 2.0
+        right = (t_total - roll) / 2.0
+        thrusts = (left, right)
+        
+        # Clip to ensure valid motor values [0, 1], since sampling during training can result in invalid values
+        thrusts = np.clip(thrusts, 0, 1)
+        
+        return thrusts
 
     def get_thrusts(self, drone: Drone):
         """
@@ -61,13 +102,41 @@ class ContinuousLinearController(FlightController):
         """
         Calculate rewards for the episode
         """
-        pass
+        cfg = self.config['rewards']
+        reward = 0.0
+        
+        # Distance Penalty
+        target = drone.get_next_target()
+        dist = np.linalg.norm([target[0] - drone.x, target[1] - drone.y])
+        reward -= dist * cfg['distance_weight']
+        
+        # Hit Bonus
+        if drone.has_reached_target_last_update:
+            reward += cfg['hit_bonus']
+            
+        return reward
     
     def get_returns(self, rewards):
         """
-        Convert rewards to returns for training
+        Monty Carlo returns
+            Reverse discounted sum
+            Center mean (baseline subtrraction) - need good=positive, bad=negative for REINFORCE
+            Normalise by std - keeps returns in a range to stabilise gradients
         """
-        pass
+        gamma = self.config['hyperparameters']['gamma']
+        returns = []
+        G = 0
+        
+        # Iterate backwards: G_t = r_t + gamma * G_{t+1}
+        for r in reversed(rewards):
+            G = r + gamma * G
+            returns.insert(0, G)
+        
+        # Normalization 
+        returns = np.array(returns)
+        returns = (returns - returns.mean()) / (returns.std() + 1e-8)
+            
+        return returns
 
     def train(self):
         """
@@ -96,7 +165,7 @@ class ContinuousLinearController(FlightController):
                 
                 # Get states and actions
                 state = self.get_state(drone)
-                action = self.get_action(drone, state)
+                action = self.get_action(state, mode='train')
                 
                 # Step environment
                 thrust = self.convert_action_to_thrust(action)
