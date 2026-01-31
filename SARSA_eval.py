f7b56dff1ad1accdd419249477aca8a3d914cd41
nyang01 <ningqian.y@gmail.com>
2026-01-31 22:59:37 +0000

    add evaluation plots

--- SARSA_eval.py
+++ SARSA_eval.py
@@ -4,32 +4,32 @@ from SARSA_controller import CustomController
 
 def main():
     ctrl = CustomController()
-    ctrl.q_path = "runs/20260129_115824/q_table.npy"
+    ctrl.q_path = "runs/20260129_231302/q_table.npy"
     ctrl.load()
 
     # FIXED targets evaluation
     rep_fixed = evaluate(
         ctrl,
         eval_mode="fixed",
-        n_episodes=50,
-        seed=42,
-        max_steps=3000,
+        n_episodes=1,
+        max_steps=5000,
         bounds=ctrl.full_bounds,
-        dt=ctrl.dt,
+        log_trajectory = True,
+        traj_filename = "Evaluation/Sarsa_fast_eval_fixed_trajectory"
     )
-    save_report(rep_fixed, "Model_20260129_115824_eval_fixed_50ep_3000.json")
+    save_report(rep_fixed, "Evaluation/Sarsa_fast_eval_fixed_5000_metrics.json")
 
     # RANDOM targets evaluation
     rep_random = evaluate(
         ctrl,
         eval_mode="random",
-        n_episodes=50,
+        n_episodes=100,
         seed=43,
-        max_steps=3000,
+        max_steps=5000,
         bounds=ctrl.full_bounds,
-        dt=ctrl.dt,
+        log_trajectory = False
     )
-    save_report(rep_random, "Model_20260129_115824_eval_random_50ep_3000.json")
+    save_report(rep_random, "Evaluation/Sarsa_fast_eval_random_5000_metrics.json")
 
 if __name__ == "__main__":
     main()
