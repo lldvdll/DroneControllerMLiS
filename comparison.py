31197cc28033e029c65ecf1a545f85641936808e
lldvdll <dgs.analytics.ltd@gmail.com>
2026-02-01 02:08:42 +0000

    Comparison code tidy up

--- comparison.py
+++ comparison.py
@@ -6,42 +6,32 @@ from neural_reinforce_controller import NeuralReinforceController
 from SARSA_controller import CustomController as SARSAController
 
 
-
-
 MODELS = {
     'Heuristic': {
         'controller': 'HeuristicController',
-        'init_args': dict(),
-        'load_args': dict()
     },
     'SARSA slow': {
         'controller': 'SARSAController',
-        'init_args': dict(),
         'load_args': dict(q_path = "runs/20260129_115824/q_table.npy")
     },
     # 'SARSA fast': {
     #     'controller': 'SARSAController',
-    #     'init_args': dict(),
     #     'load_args': dict(q_path = "runs/20260129_231302/q_table.npy")
     # },
     # 'Neural Reinforce (60_reward_shaping_phase1 best)': {
     #     'controller': 'NeuralReinforceController',
-    #     'init_args': dict(test_mode=True),
     #     'load_args': dict(filename='60_reward_shaping_phase1', mode='best')
     # },
     # 'Neural Reinforce (73_reset_curriculum best)': {
     #     'controller': 'NeuralReinforceController',
-    #     'init_args': dict(test_mode=True),
     #     'load_args': dict(filename='73_reset_curriculum', mode='best')
     # },
     'Neural Reinforce (90_continue_hover best)': {
         'controller': 'NeuralReinforceController',
-        'init_args': dict(test_mode=True),
         'load_args': dict(filename='90_continue_hover', mode='best')
     },
     # 'Neural Reinforce (100_movement best)': {
     #     'controller': 'NeuralReinforceController',
-    #     'init_args': dict(test_mode=True),
     #     'load_args': dict(filename='100_movement', mode='best')
     # },
 }
@@ -54,15 +44,13 @@ def load_controller(spec):
     
     if spec['controller'] == 'HeuristicController':
         model = HeuristicController()
-        model.target_mode = 'random'
     elif spec['controller'] == 'NeuralReinforceController':
-        model = NeuralReinforceController(**spec['init_args'])
+        model = NeuralReinforceController(test_mode=True)
         model.load(**spec['load_args'])
-        model.target_mode = 'random'
     elif spec['controller'] == 'SARSAController':
         model = SARSAController()
-        model.target_mode = 'random'
         model.q_path = spec['load_args']['q_path']
+    model.target_mode = 'random'
         
     return model
     
